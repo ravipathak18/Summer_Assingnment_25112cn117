b7010894d99b61fe0de6277a96d6b94943ad194c
ravipathak18 <pathakravi368@gmail.com>
2026-06-04 19:40:17 +0530

    Add files via upload

---
+++ program10.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int upper_limit,lower_limit,prime=1,i;
+    printf("ENTER THE LOWER RANGE:");
+    scanf("%d",&lower_limit);
+    printf("ENTER THE UPPER RANGE :");
+    scanf("%d",&upper_limit);
+    
+    for(i=lower_limit;i<=upper_limit;i++)
+    {
+           
+    }
+}
